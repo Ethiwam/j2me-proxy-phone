8a072d4142f7d433fcd027428abac168c0cf0920
Ethiwam <ethiwama@gmail.com>
2024-11-07 15:24:36 -0600

    The right soft button now controls the local light on the dumbphone. Added code to make it bluetooth connect solely as client.

--- BluetoothTesterJ2ME.java
+++ BluetoothTesterJ2ME.java
@@ -8,40 +8,30 @@ public class BluetoothTesterJ2ME extends MIDlet implements CommandListener {
     private Display display;
     private Form mainForm;
     private boolean lightOn = false;
-    private StreamConnectionNotifier notifier;
     private StreamConnection connection;
     private DataInputStream inputStream;
     private DataOutputStream outputStream;
-    
+    private DiscoveryAgent discoveryAgent;
+    private String serverUrl;
+    private Command toggleLightCommand = new Command("Toggle Light", Command.ITEM, 1);
+
     public void startApp() {
         display = Display.getDisplay(this);
         mainForm = new Form("Bluetooth Connection Tester");
         updateLight();
-        
+
+        mainForm.addCommand(toggleLightCommand); // Add the "Toggle Light" command
         mainForm.setCommandListener(this);
         display.setCurrent(mainForm);
 
         new Thread(new Runnable() {
             public void run() {
                 try {
-                    // Set up the Bluetooth server to listen for connections
                     LocalDevice localDevice = LocalDevice.getLocalDevice();
-                    localDevice.setDiscoverable(DiscoveryAgent.GIAC);
-                    String url = "btspp://localhost:" + new UUID(80087355).toString() + ";name=BluetoothTesterJ2ME";
-                    notifier = (StreamConnectionNotifier) Connector.open(url);
+                    discoveryAgent = localDevice.getDiscoveryAgent();
                     
-                    // Wait for a connection from the Android app
-                    connection = notifier.acceptAndOpen();
-                    inputStream = connection.openDataInputStream();
-                    outputStream = connection.openDataOutputStream();
-
-                    while (true) {
-                        int signal = inputStream.readInt();  // Read signal from Android app
-                        if (signal == 1) {                   // Signal received to activate light
-                            lightOn = !lightOn;
-                            updateLight();
-                        }
-                    }
+                    // Start device discovery and connect to the server
+                    discoverAndConnect();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -49,26 +39,66 @@ public class BluetoothTesterJ2ME extends MIDlet implements CommandListener {
         }).start();
     }
 
+    private void discoverAndConnect() throws IOException {
+        // Ensure the MIDlet is discoverable and find Bluetooth devices
+        discoveryAgent.startInquiry(DiscoveryAgent.GIAC, new DiscoveryListener() {
+            public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
+                try {
+                    String deviceName = btDevice.getFriendlyName(false);
+                    if (deviceName.equals("BluetoothTesterApp")) { // Replace with Android device name if needed
+                        serverUrl = discoveryAgent.selectService(
+                                new UUID("0000110100001000800000805F9B34FB", false),  // Same UUID as Android server
+                                ServiceRecord.NOAUTHENTICATE_NOENCRYPT, false);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {}
+            public void inquiryCompleted(int discType) {
+                if (serverUrl != null) {
+                    try {
+                        connection = (StreamConnection) Connector.open(serverUrl);
+                        inputStream = connection.openDataInputStream();
+                        outputStream = connection.openDataOutputStream();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            public void serviceSearchCompleted(int transID, int respCode) {}
+        });
+    }
+
     protected void updateLight() {
         mainForm.deleteAll();
         mainForm.append("Signal Light: " + (lightOn ? "ON" : "OFF"));
     }
 
+    protected void toggleLight() {
+        // Toggle light on and off
+        lightOn = !lightOn;
+        updateLight();
+        
+        // Send the toggle signal to the Android app
+        sendSignal();
+    }
+
     protected void sendSignal() {
         try {
-            outputStream.writeInt(1);  // Send signal to the Android app
+            outputStream.writeInt(lightOn ? 1 : 0);  // Send the current light state to the Android app
             outputStream.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
+
     protected void pauseApp() {}
     protected void destroyApp(boolean unconditional) {}
-    
+
     public void commandAction(Command c, Displayable d) {
-        if (c.getLabel().equals("5")) {
-            sendSignal();  // Send signal when the "5" button is pressed
+        if (c == toggleLightCommand) {
+            toggleLight();  // Toggle light when "Toggle Light" command is triggered
         }
     }
 }
